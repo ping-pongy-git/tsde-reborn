c59e1b5ac3e800e2d7b0c564be438bbf78b9bd5b
creepersacl <101290047+ping-pongy-git@users.noreply.github.com>
2022-07-13 11:40:55 -0400

    add arch shell/desktop files to source code

---
+++ custom_de.sh
@@ -0,0 +1,19 @@
+xfwm4 & 
+xfce4-panel &
+xfdesktop & 
+plank
+xfce4-panel &
+xfdesktop &
+xfwm4
+xfce4-panel &
+xfdesktop &
+xfwm4
+xfce4-panel &
+xfdesktop &
+xfwm4
+xfce4-panel &
+xfdesktop &
+xfwm4
+xfce4-panel &
+xfdesktop &
+xfwm4
